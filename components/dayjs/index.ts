13f0d7c66760a17c39d5355534ddb9c6a59861e8
moneko97 <poi.nyaa@qq.com>
2024-11-16 04:08:38 +0800

    feat: 更新构建工具

--- components/dayjs/index.ts
+++ components/dayjs/index.ts
@@ -1,14 +1,14 @@
-import dayjs, { Dayjs, extend, isDayjs, locale } from 'dayjs';
+import * as dayjs from 'dayjs';
 import localeData from 'dayjs/plugin/localeData';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import weekday from 'dayjs/plugin/weekday';
 
 import 'dayjs/locale/zh-cn';
 
-extend(weekday);
-extend(localeData);
-extend(relativeTime);
-locale('zh-cn');
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(relativeTime);
+dayjs.locale('zh-cn');
 
-export { Dayjs, isDayjs, localeData };
+export * from 'dayjs';
 export default dayjs;
